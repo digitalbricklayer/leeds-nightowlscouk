6b4c721f388d9d80f376bf41e1781ba38faa1a44
Jack Hughes <jack.hughes@techteapot.com>
2025-08-26 17:22:26 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ export default {
   async fetch(request) {
     const externalHostname = "leedsnightowls.com";
 
+    // the page_id query refers to an old pages
+    const pageIdMap = new Map([
+      ["25", "https://" + externalHostname + "/gallery/"],
+      ["297", "https://" + externalHostname + "/events/"],
+    ]);
+    
     const redirectMap = new Map([
       ["/", "https://" + externalHostname + "/"],
-      ["/bulk2", "https://" + externalHostname + "/redirect3"],
-      ["/bulk3", "https://" + externalHostname + "/redirect4"],
-      ["/bulk4", "https://google.com"],
     ]);
 
     const requestURL = new URL(request.url);
     const path = requestURL.pathname;
+
+    // Redirect old page requests
+    if (path == "/") {
+      const page_id = requestURL.searchParams.get("page_id");
+      const page_id_redirect = pageIdMap.get(page_id);
+      if (page_id_redirect) {
+        return Response.redirect(page_id_redirect, 301);
+      }
+    }
+
+    // Redirect pages
     const location = redirectMap.get(path);
 
     if (location) {
       return Response.redirect(location, 301);
     }
+    
     // If request not in map, return the original request
     return fetch(request);
   },
